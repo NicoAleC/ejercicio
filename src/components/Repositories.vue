0e4b75b7e84fe0a417dbeb2b9ab1d0736ea86ecb
Nicolas Carrasco <nico.carrasco.arandia@outlook.com>
2019-05-30 12:12:27 -0400

    elementos visibles del archivo json

--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -1,28 +1,34 @@
 <template>
-    <div>
-        <h1>HOLAAAAA</h1>
-        <button v-on:click="imprimir">Imprimir</button>
+  <div>
+    <h1>HOLAAAAA</h1>
+
+    <div class="card text-white bg-dark mb-3 col-4" v-for="item in data" :key="item.name">
+      <img class="card-img" :src="item.avatar_url" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h4 class="card-name">User: {{item.name}}</h4>
+        <h4 class="card-nick">Nick: {{item.nickName}}</h4>
+        <h6 class="card-url">Url: {{item.github_url}}</h6>
+        <a :href="item.github_url" class="btn btn-primary">See Repository</a>
+      </div>
     </div>
+    <button v-on:click="imprimir">Imprimir</button>
+  </div>
 </template>
 
 <script>
-//import Data from "../assets/data.json"
-import fs from 'fs'
+import Data from "@/assets/daja.json";
 
 export default {
-    data() {
-        return {
-            //Data
-        }
-    },
-    methods: {
-        imprimir(){
-            const file = fs.readFile('../assets/data.json')
-            const data = JSON.parse(file)
-            console.log(data)
-            //console.log(require("../assets/data.json"))
-        }
-    },
-    name: 'repositories'
-}
+  data() {
+    return {
+      data: Data.users
+    };
+  },
+  methods: {
+    imprimir() {
+      console.log(this.data);
+    }
+  },
+  name: "repositories"
+};
 </script>
